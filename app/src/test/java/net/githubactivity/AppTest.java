f1513d01c1e6736fefb99b15954c74b2117ca932
Kanterov Roman <krd23u554@students.bmstu.ru>
2025-03-24 01:52:07 +0300

    fixes: unit fixes

--- app/src/test/java/net/githubactivity/AppTest.java
+++ app/src/test/java/net/githubactivity/AppTest.java
@@ -3,11 +3,14 @@ package net.githubactivity;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 
 import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void noUserTest() throws IOException, InterruptedException {
-        assertNull(GithubParser.parseGithubJson(HttpConnection.response("")));
+        try {
+            assertThrows(InterruptedIOException.class, () -> HttpConnection.response("asddsadsddas"));
+        } catch (Exception ignored) {}
     }
 }
